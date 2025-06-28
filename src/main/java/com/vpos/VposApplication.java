ce1d0b805141efcf73cd654475a3cf5787cbefb3
kimhoeyun <clnvrd@suwon.ac.kr>
2025-06-28 14:36:53 +0900

    Initial commit

---
+++ src/main/java/com/vpos/VposApplication.java
@@ -0,0 +1,13 @@
+package com.vpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VposApplication.class, args);
+	}
+
+}
