82da7f2268865e4245d4b6c38a7353ae63caa11a
kimhoeyun <clnvrd@suwon.ac.kr>
2025-08-03 17:29:22 +0900

    remove: 불필요한 로그 삭제

--- src/main/java/com/vpos/global/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/vpos/global/jwt/filter/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = extractTokenFromHeader(request);
-        log.info(token);
+
         if (token == null) {
             filterChain.doFilter(request, response);
             return;
